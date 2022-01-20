7efb0828be7876ba3a6b40a6978e8d8486fa9d6a
Amit Thakur <at7129652@gmail.com>
2022-01-20 11:49:44 +0530

    created a store using redux toolkit to fetch api

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import{ createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '50bf1238femsh4826d23c4e25cd4p1ce006jsn2ee86e6656ac'    
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
