28f4f8dca7b403b3d04c6a68b2fc0af88778a3cc
Amit Thakur <at7129652@gmail.com>
2022-01-20 11:53:01 +0530

    completed the home page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
 
 const { Title } = Typography;
 
@@ -30,6 +31,12 @@ const Homepage = () => {
         <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
         <Title level={3} className='Show-more'><Link to="/cryptocurrencies">Show more</Link></Title>
       </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className='home-title'>Latest Crypto News</Title>
+        <Title level={3} className='Show-more'><Link to="/news">Show more</Link></Title>
+      </div>
+      <News simplified />
     </>
   )
 };
